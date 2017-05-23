c613624474fbdcc663f2799cb69a5cc082ed6f34
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2017-05-23 14:03:20 -0400

    placeholder files

--- src/models/provider.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const Schema = mongoose.Schema;
-
-export const Provider = new Schema({
-    name: { type: String, required: true },
-    url: { type: String, required: true }
-});
--- src/models/subject.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import Tag from './tag.js';
-
-const Schema = mongoose.Schema;
-
-export const Subject = new Schema({
-    name: { type: String, required: true },
-    tags: [ { type: Tag, required: true } ]
-});
